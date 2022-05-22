79d91ae8034548a300eb1900d4fb2761502cbafa
Jose Chavez Guevara <josegrone181@gmail.com>
2022-05-22 23:47:12 +0200

    Autentificacion completa

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,57 +2,91 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Auth;
-use Validator;
 use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
         $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'apellidos' => 'required | string | max:50',
-            'telefono' => 'required | numeric | max:9',
-            'ciudad' => 'required | string | max:70',
-            'provincia' => 'required | string | max:70',
-            'email' => 'required|string|email|max:255|unique:users',
-            'username' => 'required | string | max:50',
-            'password' => 'required|string|min:8'
+            'nombre' => 'required | max:255',
+            'apellidos' => 'required | max:50',
+            'telefono' => 'required | max:9',
+            'ciudad' => 'required | max:70',
+            'provincia' => 'required | max:70',
+            'email' => 'required | email | max:255|unique:users,email',
+            'username' => 'required | max:50 | unique:users,username',
+            'password' => 'required | min:8'
         ]);
 
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json([
+                'errores' => $validator->messages(),
+            ]);
+        }else{
+            $user = User::create([
+                'nombre' => $request->nombre,
+                'apellidos' => $request->apellidos,
+                'telefono' => $request->telefono,
+                'ciudad' => $request->ciudad,
+                'provincia' => $request->provincia,
+                'email' => $request->email,
+                'username' => $request->username,
+                'password' => Hash::make($request->password)
+            ]);
+
+            $token = $user->createToken($user->email.'_Token')->plainTextToken;
+
+            return response()
+                ->json([
+                    'status' => 200,
+                    'message' => 'Registrado Correctamente.',
+                    'data' => $user, 
+                    'access_token' => $token, 
+                    'token_type' => 'Bearer'
+                ]);
         }
-        $user = User::create([
-            'nombre' => $request->nombre,
-            'apellidos' => $request->apellidos,
-            'telefono' => $request->telefono,
-            'ciudad' => $request->ciudad,
-            'provincia' => $request->provincia,
-            'email' => $request->email,
-            'username' => $request->username,
-            'password' => Hash::make($request->password)
-        ]);
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()
-            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
     }
+
     public function login(Request $request)
     {   
-        if (!Auth::attempt($request->only('username', 'password'))) {
-            return response()->json(['message' => 'Unauthorized'], 401);
+        $validator = Validator::make($request->all(), [
+            'username' => 'required | max:50',
+            'password' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'errores' => $validator->messages(),
+            ]);
+        }else{
+            $user = User::where('username', $request->username)->first();
+            
+            if (!$user || !Hash::check($request->password,$user->password)) {
+                return response()->json([
+                    'status' => 401,
+                    'message' => "Credenciales invalidas.",
+                ]);
+            }else{
+
+                $token = $user->createToken($user->email.'_Token')->plainTextToken;
+    
+                return response()->json([
+                    'status' => 200,
+                    'message' => "Logeado correctamente.",
+                    'data' => $user, 
+                    'access_token' => $token, 
+                    'token_type' => 'Bearer'
+                ]);
+            }
+            
         }
-        $user = User::where('username', $request['username'])->firstOrFail();
-        $token = $user->createToken('auth_token')->plainTextToken;
-        
-        return response()
-            ->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
     }
 
     public function logout()
